da4cc7f2d7a0273f7ec24dce8b8721c2f64936bf
Maneet Rana <maneet.rana@timesinternet.in>
2023-03-06 12:14:14 +0530

    working on header part

--- src/components/HeaderNav/MoreNav.tsx
+++ src/components/HeaderNav/MoreNav.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import styles from "./styles.module.scss";
 
-const MoreNav = () => {
+const MoreNav = (props) => {
+  const {nm, sec} = props.sec;
+  console.log("sec========", sec)
   return (
-    <div>
-      MOres  ssss
-    </div>
+    <>
+      <div className={styles.flt}>
+        {
+          sec.map((elm, index) => {
+            return (
+              <>
+                {elm.nm != "Brand Solutions" && <a href={elm.link} className={styles.subsec1} rel={elm.rel && elm.rel}>{elm.nm}</a>}
+                {
+                  elm.nm != "Brand Solutions" && elm.sec && elm.sec.map((elm2, index) => {
+                    return (
+                      <>
+                        <a href={elm2.link} className={styles.subsec2} rel={elm2.rel && elm2.rel}>{elm2.nm}</a> 
+                      </>
+                    )
+                  })
+                }
+              </>
+            )
+          })
+        } 
+      </div> 
+      <div className={styles.flt}>
+        {
+          sec.map((elm, index) => {
+            return (
+              <>
+                {elm.nm == "Brand Solutions" && <div className={styles.subsec1}>{elm.nm}</div>}
+                {
+                  elm.nm == "Brand Solutions" && elm.sec && elm.sec.map((elm2, index) => {
+                    return (
+                      <div key={`${index}`} className={styles.flt}>
+                        <a href={elm2.link} className={styles.subsec2} target="_blank" rel={`noreferrer ${elm2.rel && elm2.rel}`}>
+                          <img src={elm2.im} width="77" height="63" alt={elm2.nm} />
+                        </a>
+                        <h5>
+                          <a href={elm2.link} className={styles.subsec2} target="_blank" rel={`noreferrer ${elm2.rel && elm2.rel}`}>{elm2.nm}</a>
+                        </h5>
+                      </div>
+                    )
+                  })
+                }
+              </>
+            )
+          })
+        } 
+      </div> 
+    </>
   )
 }
 
--- src/components/HeaderNav/index.tsx
+++ src/components/HeaderNav/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import Service from "network/service";
 import APIS_CONFIG from "network/config.json";
+import MoreNav from "./MoreNav";
 
 const subSecNavHtml = (data, index) => {
   return (
@@ -77,28 +78,83 @@ const subSecNav = (subSectionList) => {
 }
 
 const handleHoverSecHtml = (elm) => {
+  const colArr_4 = ["13352306", "2147477890"],
+  colArr_3 = ["1715249553"],
+  subSecArr = [],
+  L1 = [],
+  L2 = [],
+  L3 = [],
+  L4 = [];
+
+  let {sec, count, msid} = elm,
+  isL1 = 0,
+  isL2 = 0,
+  isL3 = 0,
+  perCol = colArr_4.indexOf(msid) != -1 ? Math.ceil(count / 4) : colArr_3.indexOf(msid) != -1 ? Math.ceil(count / 3) : msid == 837555174 ? 16 : msid == 359241701 ? 9 : msid == 94909228 ? 6 : Math.ceil(count / 2);
+  // console.log("elm ", elm);
+   console.log("elm length", perCol);
+
+  console.log("count----", count)
+
+  sec.forEach((l1, index) => {
+    subSecArr.push({
+      secType: "subsec1",
+      elm : <div key={`subsec1_${l1.msid}_${index}`}><a href={l1.link} className={styles.subsec1}>{l1.nm}</a></div>
+    });
+    l1.sec && l1.sec.forEach((l2, index) => {
+      subSecArr.push({
+        secType: "subsec2",
+        elm: <div key={`subsec2_${l2.msid}_${index}`}><a href={l2.link} className={styles.subsec2}>{l2.nm}</a></div>
+      });
+      l2.sec && l2.sec.forEach((l3, index) => {
+        subSecArr.push({
+          secType: "subsec3",
+          elm: <div key={`subsec3_${l3.msid}_${index}`}><a href={l3.link} className={styles.subsec3}>{l3.nm}</a></div>
+        });
+      });
+    });
+  });
+  subSecArr.forEach((sec, index) => {
+
+    if (index < perCol) {
+      L1.push(sec.elm);
+    } else if ((index >= perCol) && index < (perCol * 2)) {
+      if (sec.secType == "subsec1") {
+        isL1 = 1;
+      }
+      if (isL1 == 0) {
+        L1.push(sec.elm);
+      } else {
+        L2.push(sec.elm);
+      }
+    } else if ((index >= (perCol * 2)) && index < (perCol * 3)) {
+      if (sec.secType == "subsec1") {
+        isL2 = 1;
+      }
+      if (isL2 == 0) {
+        L2.push(sec.elm);
+      } else {
+        L3.push(sec.elm);
+      }
+    } else {
+      if (sec.secType == "subsec1") {
+        isL3 = 1;
+      }
+      if (isL3 == 0) {
+        L3.push(sec.elm);
+      } else {
+        L4.push(sec.elm);
+      }
+    }
+  });
+
   return (
     <>
-      {
-        elm.sec && elm.sec.map((l1, index) => {
-          return (
-            <>
-              <div>
-                <a href={l1.link} className={styles.subsec1}>{l1.nm}</a>
-              </div>
-              {l1.sec && l1.sec.map((l2, index) => {
-                return (
-                  <>
-                    <div>
-                      <a href={l1.link} className={styles.subsec2}>{l2.nm}</a>
-                    </div>  
-                  </>
-                )
-              })}
-            </>
-          )
-        })
-      }
+      <div className={styles.flt}>{L1}</div>
+      {msid != 107115 && <div className={styles.flt}>{L2}</div>}
+      {((msid == 359241701) || (msid == 13352306) || (msid == 2147477890) || (msid == 837555174) || (msid == 1715249553)) && <div className={styles.flt}>{L3}</div>}
+      {((msid == 13352306) || (msid == 2147477890) || (msid == 837555174) || (msid == 1715249553)) && <div className={styles.flt}>{L4}</div>}
+      <div className="clr"></div>
     </>
   )
 }
@@ -135,8 +191,10 @@ const HeaderNav = (props) => {
 
   const handleHoverNav = (elm) => {
     let secName = elm.nm;
+    console.log("secName----", secName)
     switch(secName){
       case "More":
+        return <MoreNav sec={elm} />
         break;
       case "Jobs":
         break;  
