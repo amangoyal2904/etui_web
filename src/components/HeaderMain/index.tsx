3d5a02193ad7cdab9796de55de53f4bcc013f0dd
Maneet Rana <maneet.rana@timesinternet.in>
2023-05-01 00:14:17 +0530

    added

---
+++ src/components/HeaderMain/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import HeaderLogo from "../HeaderLogo";
+import HeaderNav from "../HeaderNav";
+interface PageProps {
+  page: string;
+  subsecnames: any;
+  menuData: any;
+}
+
+const Header = (props) => {
+const { page, menuData, subsecnames, sectiondetail } = props;
+console.log("subsecnames", subsecnames)
+  return (
+    <header>
+      <HeaderLogo page={page} subsecnames={subsecnames} sectiondetail={sectiondetail} />
+      <HeaderNav menuData={menuData} subsecnames={subsecnames}/>
+      {/* <pre>{JSON.stringify(props.menuData, null, 2)}</pre> */}
+    </header>
+  )
+}
+
+export default Header
