20ee6506da55d347660303afb1ce9fc711f360f2
uitraining72 <uitraining72@gmail.com>
2024-02-29 18:08:49 +0530

    in progress...

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -41,8 +41,6 @@ const Scripts: FC<Props> = ({ isprimeuser, objVc = {} }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  console.log({searchParams});
-
   const minifyJS = APP_ENV === "development" ? 0 : 1;
   const jsDomain = "https://etdev8243.indiatimes.com"; //APP_ENV === "development" ? "https://etdev8243.indiatimes.com" : "https://js.etimg.com";
   const jsIntsURL = `${jsDomain}/js_ints_web.cms?v=${objVc["js_interstitial"]}&minify=${minifyJS}&x=1`;
