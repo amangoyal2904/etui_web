d89dadf31ddf848a08ec52e4842db5640621fb43
maneet.rana <hxce9a512x46cgwg3dq9pp46on10oe@bots.bitbucket.org>
2024-06-11 12:34:33 +0530

    spotlight carousel added

--- src/components/HeaderNav/SpotlightNav.tsx
+++ src/components/HeaderNav/SpotlightNav.tsx
@@ -1,58 +1,12 @@
-import React, { FC } from 'react';
-import styles from "./styles.module.scss";
+import SpotlightNavList from './SpotlightNavList';
 
-interface SpotlightNavProps {
-  sec: {
-    nm: string;
-    link: string;
-    im: string;
-    rel?: string;
-    sec?: any[];
-  }[];
-}
 
-const SpotlightNav: React.FC<SpotlightNavProps> = ({ sec }) => {
+const SpotlightNav = ({ sec }: any) => {
   console.log("Spotlight sec", sec);
   return (
     <>
       {
-        sec?.map((value, index) => {
-          return (
-            <>
-              <div className={`${value.nm == "Innovative Solution" ? styles.Edition_carouseltwo : styles.Edition_carouselone}`}>
-                {value.nm == "Innovative Solution" && <h2>{value.nm}</h2>}
-                <ul>
-                  {
-                    value.sec?.map((value1, index1) => {
-                      return (
-                        <>
-                          <li key={index1}>
-                            {
-                              value1 && Array.isArray(value1) && value1.map((value2, index2) => {
-                                return (
-                                  <>
-                                    <div className={styles.spotlightContent} key={index2}>
-                                      <a href={value2.link}>
-                                        <img src={value2.im} width="120" height="70" alt={value2.nm} />
-                                      </a>
-                                      <a className={styles.txt} href={value2.link}>
-                                        {value2.nm}
-                                      </a>
-                                    </div>
-                                  </>
-                                )
-                              })
-                            }
-                          </li>
-                        </>
-                      )
-                    })
-                  }
-                </ul>
-              </div>
-            </>
-          )
-        })
+        sec?.map((value, index) => <SpotlightNavList data={value} />)
       }
     </>
   )
