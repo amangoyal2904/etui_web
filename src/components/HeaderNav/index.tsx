ed6d42e84d302fad559f4a811cf5163e7c382070
maneet.rana <hxce9a512x46cgwg3dq9pp46on10oe@bots.bitbucket.org>
2024-08-27 19:46:06 +0530

    working on feedback points

--- src/components/HeaderNav/index.tsx
+++ src/components/HeaderNav/index.tsx
@@ -104,7 +104,7 @@ const HeaderNav: React.FC<HeaderNavProps> = ({ menuData, subsecnames }) => {
           {sectionList?.map((data, index) => {
             return (
               <div key={`nav-l1-${index}`} className={`${styles.sec_1} ${data.hovernav ? styles.hasSubSecnav : ""}`} data-l1={data.nm} data-id={data.msid}>
-                <a itemProp="url" className={`${subsec1 == data.msid ? styles.current : ''} ${data.msid === '74912912' ? `${styles.navP} ${styles.cSprite_b}` : ''}`} href={data.link} data-ga-onclick={data.link}>
+                <a itemProp="url" className={`${subsec1 == data.msid ? styles.current : ''} ${data.msid === '74912912' ? `${styles.navP} ${styles.cSprite_b}` : ''} ${data.msid === '110737294' ? `${styles.marketData_i}` : ''}`} href={data.link} data-ga-onclick={data.link}>
                 {data.nm != 'More' && <meta content={data.nm} itemProp="name" />}
                 {data.nm != 'More' && data.nm}
                 {data.nm === 'More' && <img src="https://img.etimg.com/photo/msid-100067830/et-logo.jpg" width="4" height="16" alt="More" />}
